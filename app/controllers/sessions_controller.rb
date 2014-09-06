47edb0d4caa4e59c7742c1b92b8120bc9505bc17
Henryvw <Henryvw@gmail.com>
2014-09-06 09:04:51 +0200

    set up user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url
+      redirect_to users_path
     else
       render "new"
     end
