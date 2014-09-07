23c910af0279825b6d5efe1637a355f88b462e7f
Henryvw <Henryvw@gmail.com>
2014-09-07 17:25:07 +0200

    added paperclip gem and paperclip uploader to picture form

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.1'
 gem 'haml'
 gem 'bcrypt-ruby'
+gem 'paperclip'
 
 # Use sqlite3 as the database for Active Record
 group :development do
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,16 @@
 class Picture < ActiveRecord::Base
 belongs_to :monument
+
+has_attached_file :photo,
+  :styles => {
+    :large => "450x450>",
+    :medium => "375x375>",
+    :thumb => "100x100>"
+    },
+  :default_url => "/images/:style/missing.png"
+
+validates_attachment_presence :photo
+validates_attachment_size :photo, less_than: 10.megabytes
+validates_attachment_content_type :photo, content_type: ['image/jpeg', 'image/png']
+
 end
